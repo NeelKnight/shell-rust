92e7d650eb79000a992032da670f8cacc7428260
Neel Knight <wingconeel@gmail.com>
2025-05-04 03:25:37 +0530

    Implementing echo

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
-#[allow(unused_imports)]
 use std::io::{self, Write};
 
 fn main() {
-    //println!("Welcome to the rush (RUst SHell!");
+    println!("Welcome to the rush (RUst SHell)!");
 
     loop {
         print!("$ ");
@@ -12,11 +11,24 @@ fn main() {
         io::stdin().read_line(&mut input).unwrap();
 
         // !TODO try to reduce streaming vulnerabilites
-        let sanitised_input = input.trim();
+        let sanitised_input = sanitise_input(&input);
 
-        match sanitised_input {
-            "exit 0" => break,
+        let mut parts = sanitised_input.split_whitespace();
+
+        match parts.next() {
+            Some("exit") => match parts.next() {
+                Some(code) => {
+                    std::process::exit(code.parse::<i32>().unwrap_or(1));
+                }
+                None => std::process::exit(1),
+            },
+            Some("echo") => println!("{}", parts.collect::<Vec<&str>>().join(" ")),
+            None => continue,
             _ => println!("{}: command not found", sanitised_input),
         }
     }
+}
+
+fn sanitise_input(input: &str) -> &str {
+    input.trim()
 }
